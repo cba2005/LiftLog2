6eeec0e88e9189203ad259ff0e6de9797c063892
irismoridis <irismoridis@hotmail.com>
2014-06-08 18:56:05 -0700

    the UI is so ugly it hurts my eyes

--- src/edu/uscb/cs/cs185/LiftLog2/AddEventDialog.java
+++ src/edu/uscb/cs/cs185/LiftLog2/AddEventDialog.java
@@ -1,11 +1,15 @@
 package edu.uscb.cs.cs185.LiftLog2;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 
 /**
@@ -13,10 +17,11 @@ import android.view.Window;
  */
 public class AddEventDialog extends DialogFragment{
         public AddEventDialog() {}
-
-        @Override
+    private static String[] classesArray;
+    @Override
         public Dialog onCreateDialog(final Bundle savedInstanceState) {
             super.onCreateDialog(savedInstanceState);
+            classesArray = getResources().getStringArray(R.array.classList);
 
             // creating the fullscreen dialog
             final Dialog dialog = new Dialog(getActivity());
@@ -25,7 +30,8 @@ public class AddEventDialog extends DialogFragment{
             dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
 
 
-            return dialog;
+        return dialog;
+
         }
 
 }
